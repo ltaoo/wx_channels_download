6107fe6a041fb8b164d8bfacc5d062d4da0b38f0
ltaoo <litaowork@aliyun.com>
2025-12-01 23:33:42 +0800

    feat: supports disable redirects from the feed page to the home page

---
+++ docs/guide/config/channel.md
@@ -0,0 +1,19 @@
+---
+title: 视频号
+---
+
+# 视频号
+
+对视频号本身进行一些配置
+
+## 禁用从详情页重定向到首页
+
+默认情况下，从「文件助手」打开视频号时，会自动重定向到首页
+
+```yaml
+channel:
+  disableLocationToHome: false
+```
+
+如果想禁用这个行为，可以在配置文件中将 `disableLocationToHome` 设置为 `true`：
+
--- inject/pagespy.js
+++ inject/pagespy.js
@@ -1,23 +1,21 @@
-setTimeout(function () {
-  var defaultConfig = {
-    api: "debug.weixin.qq.com",
-    clientOrigin: "https://debug.weixin.qq.com",
-  };
-  const config = defaultConfig;
-  if (__wx_channels_config__.pagespyServerAPI) {
-    config.api = __wx_channels_config__.pagespyServerAPI;
-  }
-  if (__wx_channels_config__.pagespyServerProtocol) {
-    config.clientOrigin = __wx_channels_config__.pagespyServerProtocol + "://" + config.api;
-  }
-  try {
-    window.$pageSpy = new PageSpy({
-      ...config,
-      project: "WXChannel",
-      autoRender: true,
-      title: "WXChannel Debug"
-    });
-  } catch (err) {
-    alert(err.message);
-  }
-}, 800);
+var defaultConfig = {
+  api: "debug.weixin.qq.com",
+  clientOrigin: "https://debug.weixin.qq.com",
+};
+const config = defaultConfig;
+if (__wx_channels_config__.pagespyServerAPI) {
+  config.api = __wx_channels_config__.pagespyServerAPI;
+}
+if (__wx_channels_config__.pagespyServerProtocol) {
+  config.clientOrigin = __wx_channels_config__.pagespyServerProtocol + "://" + config.api;
+}
+try {
+  window.$pageSpy = new PageSpy({
+    ...config,
+    project: "WXChannel",
+    autoRender: true,
+    title: "WXChannel Debug"
+  });
+} catch (err) {
+  alert(err.message);
+}
