ea59dce2376933ea422143c2edbbe9a75c169870
ltaoo <litaowork@aliyun.com>
2026-02-08 20:51:40 +0800

    fix: improve compatibility with Windows 7

--- pkg/system/proxy_windows.go
+++ pkg/system/proxy_windows.go
@@ -10,20 +10,21 @@ import (
 
 func enable_proxy(args ProxySettings) error {
 	args = merge_default_settings(args)
-	path := `HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings`
+	path := `HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings`
 	proxy_server_url := fmt.Sprintf("%v:%v", args.Hostname, args.Port)
 	// # 启用代理
-	// Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
-	// Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "127.0.0.1:8080"
-	cmd := fmt.Sprintf(`Set-ItemProperty -Path "%v" -Name ProxyEnable -Value 1`, path)
-	ps := exec.Command("powershell.exe", "-Command", cmd)
-	output, err := ps.CombinedOutput()
+	// reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+	// reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "127.0.0.1:8080" /f
+
+	// 使用 reg 命令替代 powershell，以提高兼容性（支持 Win7）并提升性能
+	cmd := exec.Command("reg", "add", path, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "1", "/f")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return errors.New(fmt.Sprintf("设置系统代理时发生错误，%v\n", string(output)))
+		return fmt.Errorf("设置系统代理时发生错误，%v\n", string(output))
 	}
-	cmd = fmt.Sprintf(`Set-ItemProperty -Path "%v" -Name ProxyServer -Value %v`, path, proxy_server_url)
-	ps = exec.Command("powershell.exe", "-Command", cmd)
-	output, err = ps.CombinedOutput()
+
+	cmd = exec.Command("reg", "add", path, "/v", "ProxyServer", "/t", "REG_SZ", "/d", proxy_server_url, "/f")
+	output, err = cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("设置 HTTP 代理失败，%v", string(output))
 	}
@@ -31,12 +32,13 @@ func enable_proxy(args ProxySettings) error {
 }
 
 func disable_proxy(args ProxySettings) error {
-	path := `HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings`
+	path := `HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings`
 	// # 禁用代理
-	// Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 0
-	cmd := fmt.Sprintf(`Set-ItemProperty -Path "%v" -Name ProxyEnable -Value 0`, path)
-	ps := exec.Command("powershell.exe", "-Command", cmd)
-	output, err := ps.CombinedOutput()
+	// reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+
+	// 使用 reg 命令替代 powershell
+	cmd := exec.Command("reg", "add", path, "/v", "ProxyEnable", "/t", "REG_DWORD", "/d", "0", "/f")
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("设置 HTTP 代理失败，%v", string(output))
 	}
